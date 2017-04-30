3f46ac2817d4609b9ab024709c0f55be8bff7a7a
Neota Moe <neota.moe@gmail.com>
2017-04-30 15:42:37 -0500

    clarified comments, cleaned up code

--- public/client.js
+++ public/client.js
@@ -1,25 +1,27 @@
 $(document).ready(onReady);
 
+// items to run when document loads
 function onReady(){
   $('#addItem').on('click', addListItem);
   getListItems();
   $(document).on('click', '.completed-button', completeItem);
   $(document).on('click', '.delete-button', deleteItem);
 }
-
+// gets all items from database and displays on DOM
 function getListItems(){
-  console.log('in getListItems function');
   $.ajax({
     url: '/getListItems',
     type: 'GET',
     success: function(response){
-      console.log('in getListItems: back from server-->', response);
       $('#list').empty();
       $('#completed-items').empty();
+      // displays items on DOM based on whether items is completed or not
       for (var i = 0; i < response.length; i++) {
         if(response[i].complete===true){
+          // if item is completed, adds class 'complete', visual indicator (checkmark image) and delete button
           $('#completed-items').append('<div class="item complete" data-id='+response[i].id+'><span class="checkmark"><img id="checkmark-image" src="/checkmark.png"/></span><span class="response">'+response[i].item+'</span><button class="delete-button">Delete</button></div>');
         } else{
+          // if item not complete, appends list item with complete and delete button
           $('#list').append('<div class="item" data-id='+response[i].id+'><span class="button-span"><button class="completed-button">Completed!</button></span><span class="response">'+response[i].item+'</span><button class="delete-button">Delete</button></div>');
         }  // end else
       }  // end for loop
@@ -27,41 +29,40 @@ function getListItems(){
   });  //end ajax GET
 }  // end getListItems
 
-
+// adds new to do item (input) from DOM, sends to server and database, updates DOM
 function addListItem(){
   var listItemIn = $('#listItemIn').val();
   var listItemToSend = {
     item: listItemIn
   };
-  console.log('sending listItemToSend:', listItemToSend);
+  // send new list item to server and database in ajax call
   $.ajax({
     url: '/addListItem',
     type: 'POST',
     data: listItemToSend,
+    // reset input/form field and refresh DOM list upon success
     success: function(response){
-      console.log('in addListItem: back from server with-->', response);
       $('form').trigger('reset');
       getListItems();
     }  // end success
   });  // end ajax POST
 }  // end addListItem
 
+// deletes item from DOM and database
 function deleteItem(){
-  console.log('delete item button clicked');
+  // asks for confirmation before deleting item
   if (confirm('Did you really get this done?  Are you sure you want to delete this?')===true){
     var id = $(this).parent().data('id');
     console.log('id:',id);
     var idToSend = {
       listId: id,
     };
-    console.log('idToSend:', idToSend);
+    // send id of item to delete and refresh DOM list upon success
     $.ajax({
       type: 'DELETE',
       url:'/deleteItem/',
       data: idToSend,
       success: function(response){
-        console.log('response is-->',response);
-        console.log('delete-button for id clicked');
         getListItems();
       }  // end success
     });  // end ajax DELETE
@@ -70,22 +71,20 @@ function deleteItem(){
   }  // end else
 }  // end deleteItem
 
+// changes class to 'complete' (adding strikethrough), replaces complete button with checkmark image
 function completeItem(){
-  console.log('completed button clicked');
   var id = $(this).closest('div').data('id');
-  console.log('id:',id);
   var idToSend = {
     listId: id,
   };
+  // change class to complete
   $(this).closest('div').addClass('complete');
-  $(this).closest('span').replaceWith('<span class="checkmark"><img id="checkmark-image" src="/checkmark.png"/></span>');
-  console.log('idToSend:', idToSend);
+  // send id of completed item and refresh DOM list upon success
   $.ajax({
     type: 'POST',
     url:'/completeItem/',
     data: idToSend,
     success: function(response){
-      console.log('response from server is-->',response);
       getListItems();
     }  // end success
   });  // end ajax POST
--- server.js
+++ server.js
@@ -33,29 +33,27 @@ app.get ('/getListItems', function (req, res){
   console.log('hit get ListItems');
   // array of listItems
   var allListItems = [];
-  var completedItems = [];
   // connect to db
   pool.connect( function( err, connection, done ){
-    //check if error
+    //check if error, send error code 400 if error
     if( err ){
       console.log( err );
-      // respond with error code
-      res.send( 400 );
+      res.sendStatus( 400 );
     }// end error
     else{
       console.log('connected to db');
-      // send query for all list items in table ('list') and hold in var=resultSet
+      // send query for all items in table ('list') and hold in var=resultSet
       var resultSet = connection.query( "SELECT * from list" );
-      // convert each row into an object in the allListItems array
+      // convert each row into an object allListItems array
       // on each row, push the row into allListItems
       resultSet.on( 'row', function( row ){
         allListItems.push( row );
-      }); //end on row push
+      }); // end on row push
       // on end of query, send array of list items as response
       resultSet.on( 'end', function(){
         // close connection
         done();
-        // res.send array of list items
+        // send back array of list items
         res.send( allListItems );
       }); //end on end function
     } // end else
@@ -68,19 +66,17 @@ app.post ('/addListItem', function (req, res){
   console.log('received from client:', req.body);
   // connect to db
   pool.connect( function( err, connection, done ){
-    //check if there was an error
+    //check if error, send error code 400 if error
     if( err ){
       console.log( err );
-      // respond with error code 400
-      res.send( 400 );
+      res.sendStatus( 400 );
     }// end error
     else{
       console.log('connected to db');
-      //query to write this list item (req.body) to db
+      //query to write new list item (req.body) to db
       connection.query( "INSERT INTO list (item) VALUES($1)", [req.body.item]);
-      // close connection
+      // close connection and send status
       done();
-      // res.send
       res.sendStatus(200);
     } // end else
   }); //end pool
@@ -90,41 +86,35 @@ app.post('/completeItem/', function(req, res) {
   console.log('in POST completeItem');
   console.log('req.body ->', req.body.listId);
   pool.connect( function( err, connection, done ){
-    //check if there was an error
+    //check if error, send error code 400 if error
     if( err ){
       console.log( err );
-      // respond with error code 400
-      res.send( 400 );
+      res.sendStatus( 400 );
     }// end error
     else{
       console.log('connected to db');
       //query to write this list item (req.body) to db
       connection.query( "UPDATE list SET complete=true where id=$1", [req.body.listId]);
-      // close connection
+      // close connection and send success status
       done();
-      // res.send
       res.sendStatus(200);
     } // end else
   }); //end pool
 });  // end completeItem POST
 
 app.delete('/deleteItem/', function(req, res) {
-  console.log('in DELETE deleteItem route');
-  console.log('req.body ->', req.body.listId);
   pool.connect( function( err, connection, done ){
-    //check if there was an error
+    //check if error, send error code 400 if error
     if( err ){
       console.log( err );
-      // respond with error code 400
       res.send( 400 );
     }// end error
     else{
       console.log('connected to db');
       //query to write this list item (req.body) to db
       connection.query( "DELETE from list where id=$1", [req.body.listId]);
-      // close connection
+      // close connection and send success status code 200
       done();
-      // res.send
       res.sendStatus(200);
     } // end else
   }); // end pool
